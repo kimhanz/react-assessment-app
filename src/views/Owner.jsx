eadd717bd75524dc592e7b26172f1b43016c91c0
Kim Hanz <nanaka3@hotmail.com>
2025-08-27 13:51:56 +0700

    first commit

---
+++ src/views/Owner.jsx
@@ -0,0 +1,20 @@
+export default function Owner() {
+  return (
+    <div className="min-h-screen flex flex-col items-center pt-12 gap-y-3 w-1/2">
+      <h1 className="text-4xl font-bold text-center">99 Instructor - JSDX</h1>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/d/de/BSG_-_William_Adama_%28Edward_James_Olmos%29.jpg"
+        alt="user image"
+        className="w-64 rounded-2xl"
+      />
+      <p className="text-center">
+        <span className="font-bold">Short Bio:</span>
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        aliquid, reiciendis quisquam error, delectus odit minima hic porro sequi
+        voluptatem dolores? Similique quia totam ad ratione quas animi facilis
+        ipsum!
+      </p>
+    </div>
+  );
+}
